82a6617563a7ad4d79dfd0bbecb0c9abb04b8791
aoi318 <hamaoka3018@gmail.com>
2025-12-19 23:56:43 +0900

    initial setup with Rust/Wasm and React/Vite

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -1,13 +1,12 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, crate!");
+pub fn greet(name: &str) {
+    // web-sys を使ってブラウザの alert を出す
+    let window = web_sys::window().expect("no global `window` exists");
+    
+    // 実は web_sys の alert は「alert_with_message」という名前である場合があります
+    // または &str を要求します。以下が確実な書き方です
+    window.alert_with_message(&format!("Hello, {}! from Rust", name))
+        .expect("alert failed");
 }
